1d928c258a01450b5f15b746b53c86bea07d61ac
Jeremy Tsang <tsang.jeremy@yahoo.ca>
2014-02-10 14:02:46 -0800

    first commit

---
+++ epoll_client.c
@@ -0,0 +1,7 @@
+/*
+File: 		epoll_client.c
+Usage:		./epoll_client -c <int_connections> -d <string_data> -i <int_iterations>
+Authors:	Jeremy Tsang, Kevin Eng
+Date:		February 10, 2014
+Purpose:	COMP 8005 Assignment 2 - Comparing Scalable Servers - threads/select()/epoll()
+*/
